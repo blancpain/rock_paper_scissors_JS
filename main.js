3670f027e50d25ba5e613833c0ac154f42fd6339
Yasen Dimitrov <y_dimitrov@ymail.com>
2022-12-14 00:14:32 +0200

    Add images and styling for rock paper scissors, move update score and display round winner to separate functions

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
 const OPTIONS = ['paper', 'rock', 'scissors'];
+//containers
 const container = document.querySelector('#container');
 const results = document.createElement('div');
 const roundWinner = document.createElement('div');
 const score = document.createElement('div');
 const winner = document.createElement('div');
-results.textContent = "Current Score: "; 
 container.appendChild(results);
-
+//starting scores
 let playerScore = 0;
 let computerScore = 0;
+updateScore();
 
-score.textContent = `Player: ${playerScore}; Computer: ${computerScore}`;
-results.appendChild(score);
+function updateScore() {
+    score.textContent = `Player: ${playerScore}; Computer: ${computerScore}`;
+    results.appendChild(score);
+}
+
+function displayRoundWinner(playerSelection, computerChoice, winner) {
+    roundWinner.textContent = `Player selected ${playerSelection}. Computer selected ${computerChoice}. The result is ${winner}.`;
+    results.appendChild(roundWinner);
+}
 
 function getComputerChoice() {
     return OPTIONS[Math.floor(Math.random() * OPTIONS.length)];
@@ -40,16 +48,18 @@ function playRound() {
         const playerSelection = this.id;
         const computerChoice = getComputerChoice();
         const outcome = determineWinner(playerSelection,computerChoice);
-        const winner = outcome[0] > outcome[1] ? "A win for the human race!" : outcome[1] > outcome[0] ? " win for computers :(" : "A dead draw.";
+        const winner = outcome[0] > outcome[1] ? "a win for the human race!" : outcome[1] > outcome[0] ? "a win for computers :(" : "a dead draw.";
         playerScore += outcome[0];
         computerScore += outcome[1];
-        roundWinner.textContent = `Player selected ${playerSelection}. Computer selected ${computerChoice}. The result is ${winner}.`;
-        results.appendChild(roundWinner);
-        score.textContent = `Player: ${playerScore}; Computer: ${computerScore}`;
-        results.appendChild(score);
+        updateScore();
+        displayRoundWinner(playerSelection, computerChoice, winner);
     } else {
-        const finalWinner = playerScore > computerScore ? "Human" : "Computer";
-        winner.textContent = `Game over! The winner is ${finalWinner}. Congrats!`;
-        results.appendChild(winner);
+        const finalWinner = playerScore > computerScore ? "You" : "The computer";
+        let clearPage = function () {
+            let clearElem = document.body;
+            clearElem.setAttribute('style', 'display: flex; justify-content: center; align-items: center; color: blue; font-size: 70px');
+            clearElem.innerHTML = `Game over! ${finalWinner} won.`;
+        }
+        clearPage();
     }
 }
