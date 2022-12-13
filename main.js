173287549976c1de4e04f302f5cf2032638db6ee
Yasen Dimitrov <y_dimitrov@ymail.com>
2022-12-13 18:45:17 +0200

    Add score count DOM updates and logic for final winner to playRound func
    The playRound function looks at the scores and if any of them are > 4 the game stops and the final winner is shown. Scores are updated based on the outcome of the determineWinner function that returns an array (the array's values depend on who won the round array[0] contains the human score and array[1] containsthe computer score; draw returns [0,0]).

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 const OPTIONS = ['paper', 'rock', 'scissors'];
+const container = document.querySelector('#container');
+const results = document.createElement('div');
+const roundWinner = document.createElement('div');
+const score = document.createElement('div');
+const winner = document.createElement('div');
+results.textContent = "Current Score: "; 
+container.appendChild(results);
 
+let playerScore = 0;
+let computerScore = 0;
+
+score.textContent = `Player: ${playerScore}; Computer: ${computerScore}`;
+results.appendChild(score);
 
 function getComputerChoice() {
     return OPTIONS[Math.floor(Math.random() * OPTIONS.length)];
@@ -7,13 +19,13 @@ function getComputerChoice() {
 
 function determineWinner(playerChoice, computerChoice) {
     if (playerChoice === computerChoice) {
-        return "Draw";
+        return [0,0];
     } else if (playerChoice === 'paper') {
-        return computerChoice === 'scissors' ? "Lose" : computerChoice === 'rock' ? "Win" : "Lose";
+        return computerChoice === 'scissors' ? [0,1] : computerChoice === 'rock' ? [1,0] : [0,1];
     } else if (playerChoice === 'rock') {
-        return computerChoice === 'scissors' ? "Win" : computerChoice === 'paper' ? "Lose" : "Win";
+        return computerChoice === 'scissors' ? [1,0] : computerChoice === 'paper' ? [0,1] : [1,0];
     } else if (playerChoice === 'scissors') {
-        return computerChoice === 'paper' ? "Win" : computerChoice === 'rock' ? "Lose" : "Win";
+        return computerChoice === 'paper' ? [1,0] : computerChoice === 'rock' ? [0,1] : [1,0];
     }
 }
 
@@ -24,13 +36,20 @@ buttons.forEach( (button) => {
 });
 
 function playRound() {
-    const playerSelection = this.id;
-    const computerChoice = getComputerChoice();
-    const outcome = determineWinner(playerSelection,computerChoice);
+    if (playerScore < 4 && computerScore < 4) {
+        const playerSelection = this.id;
+        const computerChoice = getComputerChoice();
+        const outcome = determineWinner(playerSelection,computerChoice);
+        const winner = outcome[0] > outcome[1] ? "A win for the human race!" : outcome[1] > outcome[0] ? " win for computers :(" : "A dead draw.";
+        playerScore += outcome[0];
+        computerScore += outcome[1];
+        roundWinner.textContent = `Player selected ${playerSelection}. Computer selected ${computerChoice}. The result is ${winner}.`;
+        results.appendChild(roundWinner);
+        score.textContent = `Player: ${playerScore}; Computer: ${computerScore}`;
+        results.appendChild(score);
+    } else {
+        const finalWinner = playerScore > computerScore ? "Human" : "Computer";
+        winner.textContent = `Game over! The winner is ${finalWinner}. Congrats!`;
+        results.appendChild(winner);
+    }
 }
-
-
-const container = document.querySelector('#container');
-const results = document.createElement('div');
-results.textContent = "Current Score: "
-container.appendChild(results);
